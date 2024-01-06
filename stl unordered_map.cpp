ec77bf8cb979bb0a083a6c3b1df28540c44391d5
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-01-06 11:20:39 +0530

    Third Commit with unordered map

--- stl unordered_map.cpp
+++ stl unordered_map.cpp
@@ -12,5 +12,14 @@ int  main(){
         int value = it.second;
         cout << key << " - " << value << endl;
     }
+    cout << endl;
+    // Copy Map
+    unordered_map <string,int> umpp2(umpp.begin(),umpp.end());
+    cout << "Key - Value" << endl;
+    for(const auto &it : umpp){
+        string key = it.first;
+        int value = it.second;
+        cout << key << " - " << value << endl;
+    }
     return 0;
 }
